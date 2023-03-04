6a448607825c619bab2ac6f2c647599491e100bb
weiwiz <weiwiz@poche.fi>
2023-03-04 01:33:31 +0100

    added covalent to fetch aUSDC balance on mumbai

--- src/components/TokenLine.tsx
+++ src/components/TokenLine.tsx
@@ -1,12 +1,18 @@
+import axios from "axios";
 import { utils } from "ethers";
-import { useEffect } from "react";
-import { useAccount, useContractRead, useNetwork } from "wagmi";
+import { useEffect, useState } from "react";
+import {
+  useAccount,
+  useContractRead,
+  useNetwork,
+  useSendTransaction,
+} from "wagmi";
 import { erc20ABI } from "wagmi";
 import tokens from "../tokens.json";
 
 interface ITokenLine {
   asset: string;
-  sendBalance: (balance: number) => void;
+  sendBalance: (balance: string) => void;
 }
 
 interface Token {
@@ -20,24 +26,35 @@ const getTokenFromName = (name: string): Token | undefined => {
 
 const TokenLine = ({ asset, sendBalance }: ITokenLine) => {
   const { address } = useAccount();
-  const { chain, chains } = useNetwork();
-  const { data, error, isLoading } = useContractRead({
-    address: getTokenFromName(asset)?.chains[chain?.id!],
-    abi: erc20ABI,
-    functionName: "balanceOf",
-    args: [address!],
-    chainId: chain?.id,
-  });
+  const [balance, setBalance] = useState<string>("");
 
   useEffect(() => {
-    if (data) sendBalance(parseFloat(utils.formatUnits(data.toString())));
-  }, [data]);
+    if (balance) sendBalance(balance);
+  }, [balance]);
+
+  useEffect(() => {
+    getBalanceFromCovalent();
+  }, [address]);
+
+  const getBalanceFromCovalent = async () => {
+    const res = await axios.get(
+      `https://api.covalenthq.com/v1/matic-mumbai/address/${address}/balances_v2/?key=${process
+        .env.REACT_APP_COVALENT_API!}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setBalance(res.data.data.items[1].balance);
+  };
 
   return (
     <tr className="cursor-pointer border-b border-gray-300 font-bold hover:bg-[#F2F3F6]">
       <td className="p-6">{asset}</td>
-      <td className="p-4">{data?.toString()}</td>
-      <td className="p-4">${data?.toString()}</td>
+      <td className="p-4">{balance && utils.formatUnits(balance, 6)}</td>
+      <td className="p-4">${balance && utils.formatUnits(balance, 6)}</td>
     </tr>
   );
 };
